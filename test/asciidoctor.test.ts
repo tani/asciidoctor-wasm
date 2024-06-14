6ea18a0145f64933ed042082f45205d1165bf38c
Masaya Taniguchi <ta2gch@gmail.com>
2024-06-15 08:01:18 +0900

    feat: Add rouge gem and update tests
    This commit introduces the rouge gem for syntax highlighting in the
    Gemfile. The README.md file is updated to include the license
    information for rouge. The package.json version is bumped up. A new
    unit test is added to asciidoctor.test.ts to test the rouge
    highlighting feature.

--- test/asciidoctor.test.ts
+++ test/asciidoctor.test.ts
@@ -10,3 +10,29 @@ test("Test Asciidoctor WebAssembly conversion from AsciiDoc to HTML", async () =
   const expectedHtml = "<div class=\"paragraph\">\n<p>This is AsciiDoc.</p>\n</div>";
   assert.strictEqual(actualHtml, expectedHtml);
 })
+
+test("Test Asciidoctor WebAssembly conversion from AsciiDoc to HTML", async () => {
+  const path = `${__dirname}/../dist/asciidoctor.wasm`;
+  const convert = await Asciidoctor.initFromPath(path);
+  const adoc = `
+:source-highlighter: rouge
+:rouge-style: github
+
+[source,ruby]
+----
+puts "Hello, World!"
+----
+  `
+  const actualHtml = await convert(adoc, { safe: "safe" });
+  const expectedHtml =
+    '<div class="listingblock">\n'+
+    '<div class="content">\n'+
+    '<pre class="rouge highlight">'+
+    '<code data-lang="ruby">'+
+    '<span class="nb">puts</span> <span class="s2">"Hello, World!"</span>'+
+    '</code>'+
+    '</pre>\n'+
+    '</div>\n'+
+    '</div>'
+  assert.strictEqual(actualHtml, expectedHtml);
+})
